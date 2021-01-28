7d5df48eda08eeb7cbeb442801839d8f0964b22a
RamizSarfaraj <ramizstudies@gmail.com>
2021-01-28 22:14:20 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+
+
+def main():
+    """
+    This function helps rename a bulk of files at once.
+    """
+    try:
+        i = int(input("Enter the starting point : ")) 
+        # path of the folder
+        path = str(input("Enter the path of the folder : "))
+        # The name you want to give to those files.
+        rename = str(input("Enter the new name you want to give : "))
+        # file extension
+        extension = str(input("Enter the extension of the file :  "))
+        for filename in os.listdir(path):
+            my_dest = rename + str(i) + extension
+            my_source = path + filename
+            my_dest = path + my_dest
+            os.rename(my_source, my_dest)
+            i += 1
+    except:
+        print("\nOops! Something is wrong.")
+        print("\nPlease look for the following possible errors : \n 1. Check if '/' is present at the end of the folder path. \n 2. May the extension doesn't exist.\n\n")
+if __name__ == '__main__':
+    main()
